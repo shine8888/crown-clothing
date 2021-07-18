71db9b70e6961e3ea4fa7c86d3f925e7d1a12ad5
shine8888 <44845891+shine8888@users.noreply.github.com>
2021-07-18 16:52:01 +0700

    adding init code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import HomePage from "./homepage.component";
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;

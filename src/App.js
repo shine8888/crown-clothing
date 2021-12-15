711377ade3a17f57de6baf01c9c8213c64d250f8
shine8888 <44845891+shine8888@users.noreply.github.com>
2021-12-15 23:10:29 +0700

    add new code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import React from "react";
-import HomePage from "./homepage.component";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h2>Hast Page</h2>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 };

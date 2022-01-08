039204b69966b8211804f44dc500668528238ec0
Quang Kieu <muathuvang.8880@gmail.com>
2022-01-09 00:12:53 +0700

    Add stripe payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51Ixww5ItMAD6whGpGnHKPDC6A7FgDaMI0EA2QVqGe3xILt2wLoYBD66MOFZq0VAhyHbDVgrOQOiISqJ490gF6tjY008tDaPL6c";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Crown Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+export default StripeButton;
